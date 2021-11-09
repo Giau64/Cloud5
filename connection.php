0c1967e4f64be1b8e63831050c0bdd13cbb85867
Giau <giautngcc19047@fpt.edu.vn>
2021-11-09 13:29:08 +0700

    LENNAO

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php 
-    $conn = mysqli_connect('localhost', 'root', '', 'giau123')
-            or die("Can not connect database".mysqli_connect_error());
+    $conn = pg_connect("postgres://fgvagbwtbigacm:344dc83ac14b1ecdc8c6fbd8705bbff0ce2dc7b8a2249caec69bceb51474c3c1@ec2-34-226-18-183.compute-1.amazonaws.com:5432/d8lg8vuvtkkrvi");
+            or die("Can not connect database".pg_connect_errormessage());
 ?>
