4cb022848e35633b82488bfe4da14e7bf04a88f2
Giau <giautngcc19047@fpt.edu.vn>
2021-11-09 13:53:29 +0700

    UPDATE CONN

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php 
-    $conn = pg_connect("postgres://fgvagbwtbigacm:344dc83ac14b1ecdc8c6fbd8705bbff0ce2dc7b8a2249caec69bceb51474c3c1@ec2-34-226-18-183.compute-1.amazonaws.com:5432/d8lg8vuvtkkrvi");
+    $conn = pg_connect("host =localhost port 5432 dbname=d8lg8vuvtkkrvi user=fgvagbwtbigacm password=344dc83ac14b1ecdc8c6fbd8705bbff0ce2dc7b8a2249caec69bceb51474c3c1");
             or die("Can not connect database".pg_connect_errormessage());
 ?>
